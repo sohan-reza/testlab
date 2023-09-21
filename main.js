0dc706e0782c1466aaaaa644872b93a7a75e1037
Sohan Reza <sohanreja22@gmail.com>
2023-09-21 22:03:46 +0600

    node module

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-import data from 'file.json';
-console.log(data);
+const fs = require('fs');
+
+// Replace 'your-json-file.json' with the actual path to your JSON file
+const jsonFilePath = 'file.json';
+
+try {
+  const data = fs.readFileSync(jsonFilePath, 'utf8');
+  const jsonData = JSON.parse(data);
+  // Handle the parsed JSON data here
+  console.log(jsonData);
+} catch (error) {
+  console.error('Error reading or parsing JSON file:', error);
+}
+
